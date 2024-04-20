a0a7fcf3892ca33efee5042d63d826a9422e1d61
Kunwar-Abhishek-Singh <aksgaya6@gmail.com>
2024-04-20 16:20:22 +0530

    api url endpoint updated

--- src/API/BackendUrl.jsx
+++ src/API/BackendUrl.jsx
@@ -1,1 +1,2 @@
-export const Backend_Url= "http://localhost:3001/"
+// export const Backend_Url= "http://localhost:3001/"
+export const Backend_Url= "https://topper-s-guidance-backend.onrender.com/"
